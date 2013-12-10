d3ef54166f695908ad47ce3469cec3a0b739047d
David Black <dtinblack@gmail.com>
2013-12-10 15:29:56 +0000

    added index.html

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+var http = require('http').createServer(handler),
+    io = require('socket.io').listen(http),
+    fs = require('fs');
+
+http.listen(8080);
+console.log("server started on localhost:8080");
+  
 var SerialPort = require("serialport").SerialPort;
 var portName = "/dev/ttyACM0";
 
@@ -11,6 +18,34 @@ sp.on("error", function(err) {
      console.error("error", err);
 });
 
+
+var readData="";
+
 sp.on("data", function(data) {
+
   console.log("serial port: " + data.toString());
+
+  readData += data.toString();
+
+  io.sockets.emit("message", readData);
+
+  readData="";
+
+});
+
+
+function handler(req, res) {
+    fs.readFile(__dirname + '/index.html',
+      function( err, data) {
+         if (err) {
+           res.writeHead(500);
+           return res.end('Error loading index.html');
+      }
+      res.writeHead(200);
+      res.end(data);
+   });
+}
+
+io.sockets.on('connection', function(socket) {
+     socket.emit('message', { arduinodata: 'hello from the server' });
 });
